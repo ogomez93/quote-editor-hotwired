2a7e7aaab71e12fadbb001458bf923a17fb75f09
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-06-14 03:55:21 -0300

    Adds LineItems to LineItemDates, with nested turbo-frames

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
